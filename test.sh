2cea15096f823f3f86c49e296bb4b062fe8c1cee
Neeraj Dalal <admin@nrjdalal.com>
2025-03-24 14:14:02 +0530

    chore: tweaks to test.sh ~ 1 file 4-
    test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-(curl -fsSL -X POST -H "Authorization: Bearer " "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments" \
-  -d "{\"body\": \"Package released - [\`$PACKAGE_NAME@$RELEASE_VERSION\`]($PACKAGE_URL)\"}" && echo "🟢 Release comment added!") || true
-(curl -fsSL -X POST -H "Authorization: Bearer " "https://api.github.com/repos/$GITHUB_REPOSITORY/statuses/$GITHUB_SHA" \
-  -d "{\"state\": \"success\", \"context\": \"Package released\", \"description\": \"$PACKAGE_NAME@$RELEASE_VERSION\", \"target_url\": \"$PACKAGE_URL\"}" && echo "🟢 Release status added!") || true
